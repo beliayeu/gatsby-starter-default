477c39f01c1e83d387a5ac33d2a92e6245d13a73
Beliayeu, Andrei [JJCUS Non-J&J] <abeliaye@ITS.JNJ.com>
2020-03-19 21:03:19 +0300

    case how to reproduce issue with excluding asset from build

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,21 @@
  */
 
 // You can delete this file if you're not using it
+
+
+exports.onCreateWebpackConfig = (
+    { stage, actions },
+  ) => {
+   if (stage === 'build-javascript') {
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /\/layout\.css$/,
+              use: 'null-loader',
+            },
+          ],
+        },
+      });
+    }
+  };
